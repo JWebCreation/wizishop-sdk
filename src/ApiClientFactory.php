c8f3027337c543e05c841b3e3379950acebc9ef2
Guillaume <guillaume@jweb-creation.fr>
2018-10-15 10:44:47 +0200

    Ajout de la gestion de l'authentification via token

--- src/ApiClientFactory.php
+++ src/ApiClientFactory.php
@@ -16,32 +16,49 @@ class ApiClientFactory
 	 *
 	 * @return AuthenticatedApiClient
 	 */
-	public static function authenticate($username, $password, array $config = [])
-	{
-		$client = new Client([
-								 'base_uri' => AuthenticatedApiClient::API_URL,
-							 ]);
+    public static function authenticate($username, $password, array $config = [])
+    {
+        $client = new Client([
+            'base_uri' => AuthenticatedApiClient::API_URL,
+        ]);
 
-		try {
-			$authResponse = $client->post('v3/auth/login', [
-				'json' => [
-					'username' => $username,
-					'password' => $password
-				]
-			]);
+        try {
+            $authResponse = $client->post('v3/auth/login', [
+                'json' => [
+                    'username' => $username,
+                    'password' => $password
+                ]
+            ]);
 
-			$jsonResponse = json_decode($authResponse->getBody(), true);
+            $jsonResponse = json_decode($authResponse->getBody(), true);
 
-			$jwt = JWT::fromString($jsonResponse['token']);
+            $jwt = JWT::fromString($jsonResponse['token']);
 
-			$config['account_id'] = $jsonResponse['account_id'];
-			$config['default_shop_id'] = $jsonResponse['default_shop_id'];
+            $config['account_id'] = $jsonResponse['account_id'];
+            $config['default_shop_id'] = $jsonResponse['default_shop_id'];
 
-			$client = new AuthenticatedApiClient($jwt, $config);
+            $client = new AuthenticatedApiClient($jwt, $config);
 
-			return $client;
-		} catch (RequestException $e) {
-			throw new AuthenticationException('Authentication problem', $e->getRequest(), $e->getResponse());
-		}
-	}
+            return $client;
+        } catch (RequestException $e) {
+            throw new AuthenticationException('Authentication problem', $e->getRequest(), $e->getResponse());
+        }
+    }
+
+    public static function token($token, $account_id, $default_shop_id, array $config = [])
+    {
+        $config = [];
+        try {
+            $jwt = JWT::fromString($token);
+
+            $config['account_id'] = $account_id;
+            $config['default_shop_id'] = $default_shop_id;
+
+            $client = new AuthenticatedApiClient($jwt, $config);
+
+            return $client;
+        } catch (RequestException $e) {
+            throw new AuthenticationException('Authentication problem', $e->getRequest(), $e->getResponse());
+        }
+    }
 }
