48c68557675cde891940ba291eed85b4e49bc8f5
Jeremy <jeremy@29ter.fr>
2020-02-18 15:53:56 +0100

    Correction getCategories

--- src/AuthenticatedApiClient.php
+++ src/AuthenticatedApiClient.php
@@ -206,8 +206,8 @@ class AuthenticatedApiClient extends \GuzzleHttp\Client
      */
     public function getCategories(array $params = [])
     {
-//        return $this->getAllResultsForRoute('categories');
-		return $this->getSingleResultForRoute("categories", []);
+        return $this->getAllResultsForRoute('categories');
+//		return $this->getSingleResultForRoute("categories", []);
     }
 
 	/**
